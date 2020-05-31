22b5e3615498d08236853bbb0f2f66f93b16f84e
oyewale tayo <tayooyewale@gmail.com>
2020-06-01 00:52:50 +0100

    Updates

---
+++ precache-manifest.81b3e58b2579634076404cdcdf91b52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df42318f51f9e815c06776d88109af1",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "4afbf4ae51b2f1ee4138",
+    "url": "/monsters-roladex/static/css/main.2081ed70.chunk.css"
+  },
+  {
+    "revision": "6dc28c87a26b9ecf52e0",
+    "url": "/monsters-roladex/static/js/2.99603d65.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/monsters-roladex/static/js/2.99603d65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4afbf4ae51b2f1ee4138",
+    "url": "/monsters-roladex/static/js/main.0987663f.chunk.js"
+  },
+  {
+    "revision": "8a97a7a4486a8ea1e749",
+    "url": "/monsters-roladex/static/js/runtime-main.6d7a5881.js"
+  }
+]);
